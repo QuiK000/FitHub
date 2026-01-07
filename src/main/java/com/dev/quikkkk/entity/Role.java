6c08fd78c35ccefe8f5dd3e523f821066b71756f
QuiK000 <teamquiksteam@gmail.com>
2026-01-07 13:32:43 +0200

    fix: add unique constraint to role name and simplify Role entity
    - Add unique constraint to role_name column to prevent duplicate roles
    - Remove bidirectional ManyToMany relationship with User for cleaner separation
    - Maintain entity structure extending BaseEntity with Lombok annotations

--- src/main/java/com/dev/quikkkk/entity/Role.java
+++ src/main/java/com/dev/quikkkk/entity/Role.java
@@ -2,16 +2,12 @@ package com.dev.quikkkk.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "roles")
@@ -21,9 +17,6 @@ import java.util.Set;
 @AllArgsConstructor
 @SuperBuilder
 public class Role extends BaseEntity {
-    @Column(name = "role_name", nullable = false)
+    @Column(name = "role_name", nullable = false, unique = true)
     private String name;
-
-    @ManyToMany(mappedBy = "roles")
-    private Set<User> users = new HashSet<>();
 }
