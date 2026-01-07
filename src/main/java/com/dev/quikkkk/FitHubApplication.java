0d09685390f82dfcfcec7ecd9cd53ae7e6f4d185
QuiK000 <teamquiksteam@gmail.com>
2026-01-07 13:10:14 +0200

    chore: enhance application configuration with logging and JPA auditing
    - Add Lombok's @Slf4j annotation for logging capabilities
    - Enable JPA auditing with @EnableJpaAuditing annotation
    - Reformat code for better readability

--- src/main/java/com/dev/quikkkk/FitHubApplication.java
+++ src/main/java/com/dev/quikkkk/FitHubApplication.java
@@ -1,13 +1,17 @@
 package com.dev.quikkkk;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@Slf4j
 public class FitHubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FitHubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FitHubApplication.class, args);
+    }
 
 }
