01672a610e9241a0763b094a9b3aa948181be8b5
QuiK000 <teamquiksteam@gmail.com>
2026-01-07 12:31:51 +0200

    First commit

---
+++ src/main/java/com/dev/quikkkk/FitHubApplication.java
@@ -0,0 +1,13 @@
+package com.dev.quikkkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitHubApplication.class, args);
+	}
+
+}
