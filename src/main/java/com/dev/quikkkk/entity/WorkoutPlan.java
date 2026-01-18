7c574624dc8c756011f272c69dd8cdf41069d949
QuiK000 <teamquiksteam@gmail.com>
2026-01-18 10:51:01 +0200

    chore: add @Builder.Default to workout plan exercises collection
    - Add @Builder.Default annotation to exercises field in WorkoutPlan entity
    - Ensure Lombok builder initializes HashSet by default
    - Prevent null collections when using builder pattern
    - Maintain same cascade and orphan removal configuration

--- src/main/java/com/dev/quikkkk/entity/WorkoutPlan.java
+++ src/main/java/com/dev/quikkkk/entity/WorkoutPlan.java
@@ -12,6 +12,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -51,6 +52,7 @@ public class WorkoutPlan extends BaseEntity {
     @JoinColumn(name = "trainer_id", nullable = false)
     private TrainerProfile trainer;
 
+    @Builder.Default
     @OneToMany(mappedBy = "workoutPlan", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<WorkoutPlanExercise> exercises = new HashSet<>();
 
