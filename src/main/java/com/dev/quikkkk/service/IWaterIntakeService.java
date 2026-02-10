26f91c0fea312b81c547aea4eed0311f617a3532
QuiK000 <teamquiksteam@gmail.com>
2026-02-10 19:43:39 +0200

    feat: extend IWaterIntakeService with weekly intake query
    Changes:
    - Added getWeeklyWaterIntake() method to IWaterIntakeService:
      - Returns List<DailyWaterIntakeResponse> for weekly water consumption data
      - Will include daily summaries for the current week
      - Added import for List
    - This enables weekly water intake tracking and reporting
    
    This supports comprehensive water intake analytics with weekly progress overview.

--- src/main/java/com/dev/quikkkk/service/IWaterIntakeService.java
+++ src/main/java/com/dev/quikkkk/service/IWaterIntakeService.java
@@ -4,8 +4,12 @@ import com.dev.quikkkk.dto.request.LogWaterIntakeRequest;
 import com.dev.quikkkk.dto.response.DailyWaterIntakeResponse;
 import com.dev.quikkkk.dto.response.WaterIntakeResponse;
 
+import java.util.List;
+
 public interface IWaterIntakeService {
     WaterIntakeResponse createWaterIntake(LogWaterIntakeRequest request);
 
     DailyWaterIntakeResponse getTodayWaterIntake();
+
+    List<DailyWaterIntakeResponse> getWeeklyWaterIntake();
 }
