25f8a65b83d75e02c63450758ab2b6e660f11b23
QuiK000 <teamquiksteam@gmail.com>
2026-02-10 19:59:36 +0200

    feat: add MeasurementType enum for body measurements
    This enum defines standard body measurement points (neck, chest, waist, hips,
    thighs, calves, biceps, forearms, shoulders) required for the sizing system.
    It will be used to map and validate user-provided measurements.

---
+++ src/main/java/com/dev/quikkkk/enums/MeasurementType.java
@@ -0,0 +1,17 @@
+package com.dev.quikkkk.enums;
+
+public enum MeasurementType {
+    NECK,
+    CHEST,
+    WAIST,
+    HIPS,
+    THIGH_LEFT,
+    THIGH_RIGHT,
+    CALF_LEFT,
+    CALF_RIGHT,
+    BICEP_LEFT,
+    BICEP_RIGHT,
+    FOREARM_LEFT,
+    FOREARM_RIGHT,
+    SHOULDERS
+}
