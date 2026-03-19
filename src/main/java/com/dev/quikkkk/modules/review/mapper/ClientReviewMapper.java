774a33b9d67948a72a2a350f502885eb467a7d5e
QuiK000 <teamquiksteam@gmail.com>
2026-03-19 20:16:50 +0200

    feat(review): add review mapper
    Add ClientReviewMapper with toEntity() and toResponse()
    for converting between entity and DTO.

---
+++ src/main/java/com/dev/quikkkk/modules/review/mapper/ClientReviewMapper.java
@@ -0,0 +1,54 @@
+package com.dev.quikkkk.modules.review.mapper;
+
+import com.dev.quikkkk.modules.review.dto.request.CreateTrainerReviewRequest;
+import com.dev.quikkkk.modules.review.dto.response.TrainerReviewResponse;
+import com.dev.quikkkk.modules.review.entity.TrainerReview;
+import com.dev.quikkkk.modules.user.dto.response.ClientShortResponse;
+import com.dev.quikkkk.modules.user.entity.ClientProfile;
+import com.dev.quikkkk.modules.user.entity.TrainerProfile;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientReviewMapper {
+    public TrainerReview toEntity(
+            TrainerProfile trainer,
+            ClientProfile client,
+            CreateTrainerReviewRequest request
+    ) {
+        return TrainerReview.builder()
+                .reviewer(client)
+                .trainer(trainer)
+                .rating(request.getRating())
+                .comment(request.getComment())
+                .professionalismRating(request.getProfessionalismRating())
+                .knowledgeRating(request.getKnowledgeRating())
+                .communicationRating(request.getCommunicationRating())
+                .motivationRating(request.getMotivationRating())
+                .visible(true)
+                .edited(false)
+                .createdBy(client.getId())
+                .build();
+    }
+
+    public TrainerReviewResponse toResponse(TrainerReview review) {
+        return TrainerReviewResponse.builder()
+                .id(review.getId())
+                .reviewer(
+                        ClientShortResponse.builder()
+                                .clientId(review.getReviewer().getId())
+                                .clientFirstname(review.getReviewer().getFirstname())
+                                .clientLastname(review.getReviewer().getLastname())
+                                .build()
+                )
+                .rating(review.getRating())
+                .comment(review.getComment())
+                .professionalismRating(review.getProfessionalismRating())
+                .knowledgeRating(review.getKnowledgeRating())
+                .communicationRating(review.getCommunicationRating())
+                .motivationRating(review.getMotivationRating())
+                .createdAt(review.getCreatedDate())
+                .edited(review.isEdited())
+                .editedAt(review.getEditedAt())
+                .build();
+    }
+}
