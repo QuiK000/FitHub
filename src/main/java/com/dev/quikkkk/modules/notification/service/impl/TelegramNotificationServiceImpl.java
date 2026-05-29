29f696f2fc3e27db5b4a0de7abcf27d322409b29
QuiK000 <teamquiksteam@gmail.com>
2026-05-29 12:56:17 +0300

    feat(telegram): add retry mechanism and improve error handling for notifications

--- src/main/java/com/dev/quikkkk/modules/notification/service/impl/TelegramNotificationServiceImpl.java
+++ src/main/java/com/dev/quikkkk/modules/notification/service/impl/TelegramNotificationServiceImpl.java
@@ -4,7 +4,11 @@ import com.dev.quikkkk.modules.notification.service.ITelegramNotificationService
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
@@ -22,6 +26,10 @@ public class TelegramNotificationServiceImpl implements ITelegramNotificationSer
     private final RestTemplate restTemplate;
 
     @Override
+    @Retryable(
+            retryFor = { Exception.class },
+            backoff = @Backoff(delay = 2000, multiplier = 2)
+    )
     public void sendNotification(String chatId, String message) {
         if (chatId == null || chatId.trim().isEmpty()) {
             log.warn("Telegram chat ID is empty. Cannot send message: {}", message);
@@ -32,14 +40,21 @@ public class TelegramNotificationServiceImpl implements ITelegramNotificationSer
         Map<String, String> requestBody = Map.of(
                 "chat_id", chatId,
                 "text", message,
-                "parse_mode", "HTML"
+                "parse_mode", "HTML",
+                "disable_web_page_preview", "true"
         );
 
         try {
-            restTemplate.postForEntity(url, requestBody, String.class);
-            log.info("Telegram notification sent successfully to chat {}", chatId);
+            ResponseEntity<String> response =  restTemplate.postForEntity(url, requestBody, String.class);
+            log.info("Telegram notification sent to chat {}", chatId);
+        } catch (HttpClientErrorException.Forbidden e) {
+            log.warn("User {} blocked the bot. Needs to be unlinked", chatId);
+            // TODO
+        } catch (HttpClientErrorException.BadRequest e) {
+            log.error("Telegram API BadRequest for chat {}: {}. Message: {}", chatId, e.getResponseBodyAsString(), message);
         } catch (Exception e) {
-            log.error("Failed to send notification to chat {}: {}", chatId, e.getMessage());
+            log.warn("Failed to send telegram msg to {}, retrying... Error: {}", chatId, e.getMessage());
+            throw e;
         }
     }
 }
