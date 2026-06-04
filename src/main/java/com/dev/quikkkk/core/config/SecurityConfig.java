a4208afb904ba115d60e90c984c7672e47ac065f
QuiK000 <teamquiksteam@gmail.com>
2026-06-04 18:53:33 +0300

    refactor(security): use Customizer.withDefaults() for CORS configuration

--- src/main/java/com/dev/quikkkk/core/config/SecurityConfig.java
+++ src/main/java/com/dev/quikkkk/core/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -56,8 +57,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         return http
-                .cors(cors -> {
-                })
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint((request,
