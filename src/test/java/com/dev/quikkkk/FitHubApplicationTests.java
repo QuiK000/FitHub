34d1e880610e383e933072205fbc9e8b5cb0d4f9
QuiK000 <teamquiksteam@gmail.com>
2026-06-04 18:55:18 +0300

    test: extend FitHubApplicationTests with integration test base and test profile

--- src/test/java/com/dev/quikkkk/FitHubApplicationTests.java
+++ src/test/java/com/dev/quikkkk/FitHubApplicationTests.java
@@ -1,10 +1,13 @@
 package com.dev.quikkkk;
 
+import com.dev.quikkkk.integration.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class FitHubApplicationTests {
+@ActiveProfiles("test")
+class FitHubApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
